56d503bcca40420aeeee7f15a0c3e359168b072b
sp1rexxnxxsp1rexxnxx <41951443+sp1rexxnxxsp1rexxnxx@users.noreply.github.com>
2019-01-06 16:09:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('531445108609122305');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`#ER Voice: ${currentSize}`);
+  if (!size) return channel.setName(`#M8 Voice: ${currentSize}`);
   if (currentSize !== size) channel.setName(`#M8 Voice:${currentSize}`);
 });
 
